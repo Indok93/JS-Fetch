781a1f79e17d2c717550c6e8b778dd8e34933d04
Indok93 <55687559+Indok93@users.noreply.github.com>
2022-09-14 15:09:52 +0200

    updated 14/09

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,31 +13,70 @@ const getData = () => {
         // console.log("data", data.items);
 
         apiData.push(...data.items);
-        console.log("apiData", apiData);
+        // console.log("apiData", apiData);
     })
     .catch((error) => {
         console.error(error);
     })
     .finally(() => {
-        apiData.map((goals, i) => renderContent(goals, i));
+        apiData.map((card, i) => renderContent(card, i));
     })
 }
 
-const renderContent = (goals, i) => {
-    // console.log("goals", goals);
+const renderContent = (card, i) => {
+    // console.log("goals", goals.color);
 
     document.getElementById("mother").innerHTML += `
-    <figure class="card">
-        <div class="container">
-            <img src='data:image/svg+xml; utf8, ${goals.icon}' alt='icon'>
-            <h4>${goals.title}</h4>
+    <figure class="card" onclick='getDetail(${card.id})' style='background-color: #${card.color}'>
+        <div class="card-container">
+            <div class="card-top">
+                <div class="number">${card.id}</div>
+                <h4>${card.title}</h4>
+            </div>
+            <div class ="card-middle">
+                <div class="card-img">
+                    <img src='data:image/svg+xml; utf8, ${card.icon}' alt='icon'>
+                </div>
+            </div>
         </div>
     </figure>`;
 };
 
-// const renderHair = (hair) => {
-//     const{, } = hair;
-//     return `<p>${} - ${}</p>`
-// }
+
 
 getData();
+
+const getDetail = (id) => {
+    let idData = "";
+    const endPoint = `https://api.mediehuset.net/sdg/goals/${id}`;
+
+    fetch(endPoint)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        // console.log("data", data.item);
+        idData = data.item;
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+    .finally(() => {
+        renderDetails(idData);
+    })
+}
+
+const renderDetails = (detailData) => {
+    const{byline, description, icon, image, title, color} = detailData;
+    document.getElementById("mother").innerHTML = `
+    <article class="card-detail" style='background-color: #${color}'>
+        <h1>${title}</h1>
+        <h2>${byline}</h2>
+        <img src='data:image/svg+xml; utf8, ${icon}' alt='icon' class="icons">
+        <img src="${image}" alt="image" class="images">
+        <p>${description}</p>
+        <button>Tilbage</button>
+    </article>`;
+}
+
+
